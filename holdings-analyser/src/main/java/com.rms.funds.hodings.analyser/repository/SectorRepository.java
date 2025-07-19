79d157504c9fa52032da63a2f1ed88d9c18ef020
ramesh <ramesh.gate69@gmail.com>
2025-07-19 17:12:05 +0530

    added sector, industry and stock info tables.

---
+++ holdings-analyser/src/main/java/com.rms.funds.hodings.analyser/repository/SectorRepository.java
@@ -0,0 +1,9 @@
+package com.rms.funds.hodings.analyser.repository;
+
+import com.rms.funds.hodings.analyser.entity.SectorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectorRepository extends JpaRepository<SectorEntity, Long> {
+}
