8a5effde2a0a6df86abe98268615599f2c743e65
ramesh <ramesh.gate69@gmail.com>
2025-09-10 01:03:19 +0530

    added holding extractor job

---
+++ holdings-analyser/src/main/resources/db/migration/V59__update_hsbc_date_mapper_config_dml.sql
@@ -0,0 +1,3 @@
+ update mutual_fund_config
+ set date_mapper_2 = 'dd-MMM-yyyy'
+ where download_url like 'https://www.assetmanagement.hsbc.co.in/en/mutual-funds/investor-resources/-/media/files/attachments/india/mutual-funds/portfolios/document%'
