f17727379fe73ecac45df82a2d78af80b43a703c
ramesh <ramesh.gate69@gmail.com>
2025-10-15 20:52:36 +0530

    updated docker.

--- holdings-analyser/src/main/java/com.rms.funds.hodings.analyser/schedulers/BhavcopyScheduler.java
+++ holdings-analyser/src/main/java/com.rms.funds.hodings.analyser/schedulers/BhavcopyScheduler.java
@@ -3,21 +3,19 @@ package com.rms.funds.hodings.analyser.schedulers;
 // BhavcopyScheduler.java
 import com.rms.funds.hodings.analyser.service.BhavcopyDownloaderService;
 import com.rms.funds.hodings.analyser.service.BhavcopyParserService;
-import com.rms.funds.hodings.analyser.service.BhavcopyService;
-import com.rms.funds.hodings.analyser.service.impl.BseBhavcopyService;
 import com.rms.funds.hodings.analyser.service.impl.NseBhavcopyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.io.File;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BhavcopyScheduler implements CommandLineRunner {
@@ -26,35 +24,35 @@ public class BhavcopyScheduler implements CommandLineRunner {
     private final BhavcopyParserService parser;
     private final NseBhavcopyService nseBhavcopyService;
 
-    // Run every weekday at 7:30 PM IST (Mon-Fri)
-    @Scheduled(cron = "0 30 19 * * MON-FRI", zone = "Asia/Kolkata")
+    // Run every weekday at 8:10 PM IST (Mon-Fri)
+    @Scheduled(cron = "0 10 21 * * MON-FRI", zone = "Asia/Kolkata")
     public void fetchAndStoreBhavcopy() {
         try {
-            LocalDate yesterday = LocalDate.now().minusDays(1);
-            File csvFile = downloader.downloadBhavcopy(yesterday);
+            LocalDate today = LocalDate.now();
+            File csvFile = downloader.downloadBhavcopy(today);
             parser.parseAndSave(csvFile);
-            System.out.println("✅ Stored bhavcopy for " + yesterday);
+            log.info("✅ Stored bhavcopy for {}", today);
         } catch (Exception e) {
-            System.err.println("❌ Error in BhavcopyScheduler: " + e.getMessage());
+            log.error("❌ Error in BhavcopyScheduler: {}", e.getMessage());
         }
     }
 
     @Override
     public void run(String... args) throws Exception {
-        //fetchAndStoreBhavcopy();
+            fetchAndStoreBhavcopy();
 
-        for (LocalDate date : getLast6MonthsWeekdays()) {
-            System.out.println("Starting the date : "+date.format(DateTimeFormatter.ISO_DATE));
-            try {
-              //  LocalDate yesterday = LocalDate.now().minusDays(1);
-                File csvFile = downloader.downloadBhavcopy(date);
-                parser.parseAndSave(csvFile);
-                System.out.println("✅ Stored bhavcopy for " + date);
-            } catch (Exception e) {
-                System.err.println("❌ Error in BhavcopyScheduler: " + e.getMessage()+ " for date "+date.format(DateTimeFormatter.ISO_DATE));
-            }
-            System.out.println("End the date : "+date.format(DateTimeFormatter.ISO_DATE));
-        }
+//        for (LocalDate date : getLast6MonthsWeekdays()) {
+//            System.out.println("Starting the date : "+date.format(DateTimeFormatter.ISO_DATE));
+//            try {
+//              //  LocalDate yesterday = LocalDate.now().minusDays(1);
+//                File csvFile = downloader.downloadBhavcopy(date);
+//                parser.parseAndSave(csvFile);
+//                System.out.println("✅ Stored bhavcopy for " + date);
+//            } catch (Exception e) {
+//                System.err.println("❌ Error in BhavcopyScheduler: " + e.getMessage()+ " for date "+date.format(DateTimeFormatter.ISO_DATE));
+//            }
+//            System.out.println("End the date : "+date.format(DateTimeFormatter.ISO_DATE));
+//        }
     }
 
     public List<LocalDate> getLast6MonthsWeekdays() {
